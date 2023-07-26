6212d2d164dcd7ed6c0ab041029a686fce8bda39
Guilherme Moreira <guilherme.moreira@compasso.com.br>
2023-07-26 11:06:32 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata = {
+    title: "Rebeca Lima",
+    description: "Nutricionista | Personal Chef",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body className={montserrat.className}>{children}</body>
+        </html>
+    );
+}
